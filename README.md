a2e86f9c03b9669f96a950483a8860c369a66865
vr-varad <varadgupta21@gmail.com>
2025-02-01 21:05:15 +0530

    intial setup

---
+++ README.md
@@ -0,0 +1,10 @@
+# Remote Code Execution Engine - CodeBlaster_3000
+
+## Tech Stack
+- NodeJs
+- Docker
+- Redis
+
+## Design
+
+<img src="./images/design.png" alt="design">
---
+++ src/server/controller/execution.controller.ts
@@ -0,0 +1,10 @@
+import { NextFunction, Request, Response } from "express";
+
+const submitCodeController = (req: Request, res: Response, next: NextFunction) => { };
+
+const checkResultController = (req: Request, res: Response, next: NextFunction) => { };
+
+
+export {
+    submitCodeController, checkResultController
+}
